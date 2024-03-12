257f381d8bf37d5cb3db03726f9aa0d5a88bb0e7
Vivian247 <144880443+Vivian247@users.noreply.github.com>
2024-03-12 12:14:36 -0400

    Sign In

--- hydraWebApp/src/pages/LandingPages/SignIn/index.js
+++ hydraWebApp/src/pages/LandingPages/SignIn/index.js
@@ -30,12 +30,74 @@ import routes from "routes";
 
 // Images
 import bgImage from "assets/images/drink.jpg";
+import { Alert } from "@mui/material";
 
 function SignInBasic() {
+  const backendUrl = 'http://localhost:3000'
   const [rememberMe, setRememberMe] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  
+    const handleSetRememberMe = () => setRememberMe(!rememberMe);
 
-  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+  const signInHandler = async (data) => {
+    console.log('iiiii', data)
+    try {
+        const response = await fetch(`${backendUrl}/users/signin`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
 
+        if (!response.ok) {
+            const resData = await response.json();
+            // Check for the duplicate key error
+            if (resData.error && resData.error.includes("duplicate key")) {
+                throw new Error("A user with this username or email already exists.");
+            } else {
+                throw new Error(resData.message || "Unable to create user. Please try again.");
+            }
+        }
+        
+        //setError(''); // Clear any existing error
+        console.log('lllll', response)
+        return response.data;
+    } catch (error) {
+        console.error(error.message);
+       // setError("A user with this username or email already exists."); // Example error message
+        return false;
+    }
+};
+
+  const handleFormChange = (e) => {
+    setError('')
+    if(e.target.type === 'email') {
+      setEmail(e.target.value)
+    }
+
+    if(e.target.type === 'password') {
+      setPassword(e.target.value)
+    }
+  }
+
+  const handleSignIn = async () => {
+    setError('')
+    if(!email || !password) {
+      setError('email and Password Required')
+      return
+    }
+
+    const user = await signInHandler({
+      email, password
+    })
+    if(!user) {
+      setError('Cannot sign in user, server error')
+      return
+    }
+  }
   return (
     <>
       <DefaultNavbar
@@ -68,6 +130,8 @@ function SignInBasic() {
         }}
       />
       <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={2}>
+
+
         <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
           <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
             <Card>
@@ -104,12 +168,13 @@ function SignInBasic() {
                 </Grid>
               </MKBox>
               <MKBox pt={4} pb={3} px={3}>
+                {error && <Alert severity="error">{error}</Alert> }
                 <MKBox component="form" role="form">
                   <MKBox mb={2}>
-                    <MKInput type="email" label="Email" fullWidth />
+                    <MKInput type="email" label="Email" fullWidth onChange={handleFormChange} />
                   </MKBox>
                   <MKBox mb={2}>
-                    <MKInput type="password" label="Password" fullWidth />
+                    <MKInput type="password" label="Password" fullWidth onChange={handleFormChange} />
                   </MKBox>
                   <MKBox display="flex" alignItems="center" ml={-1}>
                     <Switch checked={rememberMe} onChange={handleSetRememberMe} />
@@ -124,7 +189,7 @@ function SignInBasic() {
                     </MKTypography>
                   </MKBox>
                   <MKBox mt={4} mb={1}>
-                    <MKButton variant="gradient" color="info" fullWidth>
+                    <MKButton variant="gradient" color="info" fullWidth onClick={(e) => handleSignIn(e)}>
                       sign in
                     </MKButton>
                   </MKBox>
@@ -132,15 +197,15 @@ function SignInBasic() {
                     <MKTypography variant="button" color="text">
                       Don&apos;t have an account?{" "}
                       <MKTypography
-  component={Link}
-  to="/signup"
-  variant="button"
-  color="info"
-  fontWeight="medium"
-  textGradient
->
-  Sign up
-</MKTypography>
+                        component={Link}
+                        to="/signup"
+                        variant="button"
+                        color="info"
+                        fontWeight="medium"
+                        textGradient
+                      >
+                        Sign up
+                      </MKTypography>
                     </MKTypography>
                   </MKBox>
                 </MKBox>
